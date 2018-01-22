6d498b28ab3a360653f8734d784442883d3a40ec
Abhishek Trivedi <atrivedi397@outlook.com>
2018-01-22 22:01:21 +0530

    Updated And Working Properly

--- Finding_Prime_In_Array.cpp
+++ Finding_Prime_In_Array.cpp
@@ -1,85 +1,92 @@
 #include <iostream>
+using namespace std ;
 
-using namespace std;
+int num , j = 0 ,k =0 , n  ;
 
-int main ()
+int main()
 {
-    int n=0,verified, counter1 =0, counter2=0;
+    int verified_num;      //variable for handling the returns of check_prime()
+    int check_prime(int);  //function declaration
 
-    cout<<"\n How many numbers you want to check ?\n";
-    int prime(int number);
-    cin>>n;
-    int num_array[n] , prime_array[n] , non_prime_array[n];
-    cout<<"\n Please enter the number to check for prime \n";
-    for(int i=0; i < n; i++)
+    cout<<"\nHow many numbers you want to check for prime ?\n";
+    cin>>n;     //taking the no. of inputs
+
+    int storing_array[n],prime_array[k],non_prime_array[j];         //arrays for storing
+
+    cout<<"\n Enter the the numbers one by one .\n\n Please Enter The Numbers Greater Than 3 Only \n\n ";
+
+    for(int i = 0; i< n; i++)
     {
-        cout<<"Enter the "<<i+1<<"th element.\n";
-        cin>>num_array[i];
+        //storing the elements in array
+        cin>>num;
+        if (num != 1 && num != 2 && num != 3)
+            storing_array[i] = num;
+        else {
+            // checking if the number provided is 1 or 2 or 3
+            cout << " 1 is neither prime nor composite while 2 is the only prime number ,3 is also prime . Please enter other than 1 , 2 , 3 .\n";
+            --i;
+        }
     }
 
-   for(int i=0; i<=n; i++)
-   {
-      verified = prime(num_array[i]);
-       if(verified == 1)
-       {
-            non_prime_array[i] = num_array[i];
-           counter1++;
-       }
-       else if(verified == 2)
-       {
-           prime_array[i] = num_array[i];
-           counter2++;
-       }
-   }
+    //loop to check the numbers and storing
+    for(int i = 0; i < n; i++)
+    {
+        verified_num = check_prime(storing_array[i]);
 
-    cout<<"\n Prime Numbers Are \n\n";
+        if (verified_num == 1)
+        {
+            non_prime_array[j] = storing_array[i];
+            j++;
+        }
+        else
+        {
+            prime_array[k] = storing_array[i];
+            k++;
+        }
+    }
 
-    for (int i= 0; i<= counter2; i++)
+
+    //showing the prime and non prime numbers
+    cout<<"\n\n The Non Prime Numbers Are :\n";
+
+    for(int i = 0; i < j; i++ )
+    {
+        cout<<non_prime_array[i]<<"\t";
+    }
+
+    cout<<"\n\n The Prime Numbers Are : \n";
+
+    for(int i = 0; i < k; i++)
     {
         cout<<prime_array[i]<<"\t";
     }
 
-    cout<<"\nNon Prime Numbers Are \n\n";
-
-    for(int i= 0;i<= counter1; i++)
-    {
-       cout<<non_prime_array[i]<<"\t";
-    }
     return 0;
+
 }
 
-int prime(int number)
+int check_prime(int a)               // function to check prime
 {
-    int solution = 0, one = 1, two = 2;
-    if (number == 1 )
+    int answer = 0;
+    int half = a/2;                 // checking by dividing the number into half
+    for(int i = 2; i <= half ; i++)
     {
-        cout<<"\n 1 is neither prime nor composite.\n";
+        if((a % i) == 0)
+        {
+            answer = 1;
+            break;                    //denotes the number is not prime
+        }
+    }
+
+    if(answer == 1)
+    {
+        //returning for storing in non prime array
+        return answer;
     }
     else
     {
-        int counter = number/2;
-        for(int i = 2; i<= counter; i++)
-        {
-            if(number%i == 0)
-            {
-                solution =2;
-            }
-        }
-        if( solution== 2)
-        {
-            cout<<"\nNot a prime\n";
-            return one;
-        }
-        else
-        {
-            cout<<"\nIt Is prime\n";
-            return two;
-        }
+        //returning for storing in prime array
+        answer = 2;
+        return answer;
     }
-
-
 }
-//
-// Created by Abhishek Trivedi on 20-Jan-18.
-//
-
