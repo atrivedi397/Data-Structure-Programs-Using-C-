7439958bc45d79800f81f808bad3fb8359364f5e
Abhishek Trivedi <atrivedi397@outlook.com>
2018-01-25 13:02:37 +0530

    Added a function named 'Insert_new()'

--- Working_of_an_array.cpp
+++ Working_of_an_array.cpp
@@ -5,14 +5,18 @@ class array
 {
 public:
     //function declared for inserting the elements
-    void insert();
+    void insert_initial();
+    //function to insert new
+    void insert_new();
     //function declared for deleting the elements
     void del();
+    //function to search any element
+    void search() ;
 private:
     int* storing_array;   //the array to store elements
     int flag = 0, size, no_of_elements , value ,pos , element;      //other required variables
 };
-void array::insert()      //function defined
+void array::insert_initial()      //function defined
 {
     cout<<"\nWhat is the size of your array ?\n";
     cin>>size;
@@ -49,46 +53,6 @@ void array::insert()      //function defined
     }
 
     //do-while for the insertion of new elements
-
-    do
-    {
-        cout<<"\n\nDo you want to insert more elements\n\n1. Yes\n\n2. No\n ";
-        cin>>value;
-        if (value == 1)
-        {
-            cout<<"\nAt which position do you want to insert the new element ?\n";
-            cin>>pos;
-            cout<<"\nProvide the element you want to enter : \n";
-            cin>>element;
-            if(pos <= no_of_elements)
-            {
-                for(int i = no_of_elements; i > (pos-1) ; i--)
-                {
-                    storing_array[i] = storing_array[i-1];
-                }
-                storing_array[pos-1] = element;
-                no_of_elements+=1;
-                flag = 0;
-            }
-            else
-            {
-                cout<<"\n!!!!Array is a continuous block of memory, so can't insert at position untill its previous space is filled.\n";
-                flag = 1;
-            }
-        }
-        else
-        {
-            flag = 1;
-        }
-    }while(flag == 1);           //end of do-while
-
-    cout<<"\nThe newly formed array is :\n";
-
-    //loop to display the new array after insertion
-    for(int i = 0; i < no_of_elements; i++)
-    {
-        cout<<storing_array[i]<<"  ";
-    }
     //delete []storing_array; //(if activated the delete function is not properly working )
 }
 
@@ -128,21 +92,82 @@ void array::del()
     }
 }
 
+void array::search()
+{
+    cout<<"\n\nEnter the element you want to search : \n";
+    cin>>element;
+    for(int i = 0; i < no_of_elements; i++)
+    {
+        if(storing_array[i] == element)
+        {
+            cout<<"\n\n The given element is present at index number "<<i<<" and at position "<<(i+1)<<endl;
+            break;
+        }
+    }
+}
+
+void array::insert_new()
+{
+    do
+    {
+        cout<<"\n\nDo you want to insert more elements\n\n1. Yes\n\n2. No\n ";
+        cin>>value;
+        if (value == 1)
+        {
+            cout<<"\nAt which position do you want to insert the new element ?\n";
+            cin>>pos;
+            cout<<"\nProvide the element you want to enter : \n";
+            cin>>element;
+            if(pos <= no_of_elements)
+            {
+                for(int i = no_of_elements; i > (pos-1) ; i--)
+                {
+                    storing_array[i] = storing_array[i-1];
+                }
+                storing_array[pos-1] = element;
+                no_of_elements+=1;
+                flag = 0;
+            }
+            else
+            {
+                cout<<"\n!!!!Array is a continuous block of memory, so can't insert at position untill its previous space is filled.\n";
+                flag = 1;
+            }
+        }
+        else
+        {
+            flag = 1;
+        }
+    }while(flag == 1);           //end of do-while
+
+    cout<<"\nThe newly formed array is :\n";
+
+    //loop to display the new array after insertion
+    for(int i = 0; i < no_of_elements; i++)
+    {
+        cout<<storing_array[i]<<"  ";
+    }
+}
+
 int  main()
 {
     array ob;
     int n ;
     do
     {
-        cout<<"\n\n\nSelect one of the following\n\n1. Insert In A New Array\n\n2. Delete From An Array\n(You should first enter the elements and then you can delete)\n\n3. Exit\n";
+        cout<<"\n\n\nSelect one of the following\n\n1. Insert In A New Array\n\n2. Insert In Same Array\n\n3. Search (Linear)\n\n4. Delete From An Array\n(You should first enter the elements and then you can delete)\n\n5. Exit\n";
         cin>>n;
         switch (n)
         {
-            case 1 : ob.insert();
+            case 1 : ob.insert_initial();
                 break;
-            case 2 : ob.del();
+            case 2 : ob.insert_new();
                 break;
-            case 3 : exit(0);
+            case 3 : ob.search();
+                break;
+            case 4 : ob.del();
+                break;
+            case 5 : exit(0);
             default : cout<<"\nPlease provide a valid input . \n";
         }
     }while(true);
