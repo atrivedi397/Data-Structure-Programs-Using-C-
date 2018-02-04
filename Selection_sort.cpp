dd6ea5dc3bd19db0f32184f43c872a54e50ba86c
Abhishek Trivedi <atrivedi397@outlook.com>
2018-02-04 22:43:29 +0530

    Commenting done and final

--- Selection_sort.cpp
+++ Selection_sort.cpp
@@ -4,12 +4,14 @@ using namespace std;
 class sorting
 {
 public:
+    //functions and variables
     int* arr , size;
     void input();
     void selection_sort();
     void display();
 };
 
+//definition of inputting the array
 void sorting::input()
 {
     cout<<"\nEnter the size of the array :\n";
@@ -22,6 +24,7 @@ void sorting::input()
     }
 }
 
+//definition of display
 void sorting::display()
 {
     cout<<"\nThe array is :\n";
@@ -31,22 +34,30 @@ void sorting::display()
     }
 }
 
+//definition for sorting
 void sorting::selection_sort()
 {
+    //variables
     int pos, lowest, temp;
+
+    //main loop to assign lowest element
     for(int i = 0; i < size; i++)
     {
         lowest = arr[i];
         pos = i;
+
+        //inner loop for comparing
         for(int j = i+1; j < size; j++)
         {
             if(arr[j] < lowest)
             {
+                //changing the lowest element if found lesser than index 0
                 lowest = arr[j];
                 pos = j;
             }
-        }
+        }//end of inner loop
 
+        //swapping and putting the elements in sorted manner
         temp = arr[i];
         arr[i] = arr[pos];
         arr[pos] = temp;
@@ -57,6 +68,7 @@ void sorting::selection_sort()
 int main()
 {
     cout<<"\n\nThis is a program to sort array using selection sort algorithm.\n";
+    //object declared
     sorting ob;
     int answer;
 
@@ -69,7 +81,6 @@ int main()
         ob.display();
         cout<<"\n\nDo you want to sort another array ?\n1. Yes\n2. No\n";
         cin>>answer;
-
     }while(answer == 1);
     return 0;
 }//
